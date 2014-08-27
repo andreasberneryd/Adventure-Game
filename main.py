d6f0d4f5da6ad80deb9865da6a4891c0a1431206
Elias Forsberg <forsberg.elias@gmail.com>
2014-08-27 10:07:23 +0200

    world genereras automatiskt

--- main.py
+++ main.py
@@ -11,11 +11,6 @@ from Creature import Creature
 __author__ = 'andberne'
 
 generate_world(world)
-
-world[0][0].items.append(Weapon(0, "Rusty Sword", 10))
-#world[0][0].creatures.append(Creature(0, "Monster1", 100, 100))
-#world[0][0].creatures[0].skills.append(Skill(0, "Claw Attack", 8))
-
 player = Player(world)
 
 print("Enter a direction to move or type quit to quit")
